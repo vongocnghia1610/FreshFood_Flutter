c84e711b0d76eae0ba982087dd774f94b49927e3
NguyenPhanNhatTu <nguyenphannhattu@gmail.com>
2021-12-10 01:58:38 +0700

    [Tu:screen]forgot password

--- lib/src/pages/authentication/fogot_password_page.dart
+++ lib/src/pages/authentication/fogot_password_page.dart
@@ -17,6 +17,8 @@ class ForgotPassPage extends StatefulWidget {
 }
 
 class _ForgotPassPageState extends State<ForgotPassPage> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String email;
   @override
@@ -27,89 +29,99 @@ class _ForgotPassPageState extends State<ForgotPassPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        key: _scaffoldKey,
-        drawer: Container(
-          width: 70.w,
-          child: Drawer(
-            child: DrawerLayout(),
-          ),
-        ),
-        appBar: AppBar(
-          elevation: 0,
-          leading: IconButton(
-            onPressed: () => Get.back(),
-            icon: Icon(
-              PhosphorIcons.arrow_left,
-              color: Colors.white,
-              size: 7.w,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+          key: _scaffoldKey,
+          drawer: Container(
+            width: 70.w,
+            child: Drawer(
+              child: DrawerLayout(),
             ),
           ),
-          title: Text(
-            "Quên mật khẩu",
-            style: TextStyle(fontSize: 20),
+          appBar: AppBar(
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () => Get.back(),
+              icon: Icon(
+                PhosphorIcons.arrow_left,
+                color: Colors.white,
+                size: 7.w,
+              ),
+            ),
+            title: Text(
+              "Quên mật khẩu",
+              style: TextStyle(fontSize: 20),
+            ),
           ),
-        ),
-        body: Container(
-            height: 100.h,
-            width: 100.w,
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 20.sp, bottom: 20.sp),
-                  child: Text(
-                    "Nhập Email Của bạn",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          body: Container(
+              height: 100.h,
+              width: 100.w,
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 20.sp, bottom: 20.sp),
+                    child: Text(
+                      "Nhập Email Của bạn",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5.sp),
-                  child: TextFormField(
-                    textAlign: TextAlign.start,
-                    textAlignVertical: TextAlignVertical.top,
-                    onChanged: (val) {
-                      setState(() {
-                        email = val.trim();
-                      });
-                    },
-                    decoration: const InputDecoration(
-                        labelText: "Email",
-                        // floatingLabelBehavior: FloatingLabelBehavior.always,
-                        alignLabelWithHint: true,
-                        // border: Border.all(color: Colors.blueAccent),
-                        border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.grey, width: 1.0)),
-                        fillColor: Colors.grey),
-                    maxLines: 1,
+                  Container(
+                    padding: EdgeInsets.all(5.sp),
+                    child: TextFormField(
+                      textAlign: TextAlign.start,
+                      textAlignVertical: TextAlignVertical.top,
+                      validator: (val) => val.trim().length == 0
+                          ? 'Hãy nhập email của bạn'
+                          : null,
+                      onChanged: (val) {
+                        setState(() {
+                          email = val.trim();
+                        });
+                      },
+                      decoration: const InputDecoration(
+                          labelText: "Email",
+                          // floatingLabelBehavior: FloatingLabelBehavior.always,
+                          alignLabelWithHint: true,
+                          // border: Border.all(color: Colors.blueAccent),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0)),
+                          fillColor: Colors.grey),
+                      maxLines: 1,
+                    ),
                   ),
-                ),
-                DefaultButton(
-                  btnText: "Gửi",
-                  onPressed: () {
-                    AuthenticationRepository()
-                        .forgotPassword(email)
-                        .then((value) {
-                      if (value == true) {
-                        // GetSnackBar getSnackBar = GetSnackBar(
-                        //   title: 'Gửi thành công',
-                        //   subTitle:
-                        //       'OTP đã được gửi về email hoặc điện thoại tương ứng',
-                        // );
+                  DefaultButton(
+                    btnText: "Gửi",
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        AuthenticationRepository()
+                            .forgotPassword(email)
+                            .then((value) {
+                          if (value == true) {
+                            // GetSnackBar getSnackBar = GetSnackBar(
+                            //   title: 'Gửi thành công',
+                            //   subTitle:
+                            //       'OTP đã được gửi về email hoặc điện thoại tương ứng',
+                            // );
 
-                        Get.toNamed(Routes.OTP, arguments: {"email": email});
-                        // getSnackBar.show();
-                      } else {
-                        GetSnackBar getSnackBar = GetSnackBar(
-                          title: 'Email không tồn tại',
-                          subTitle: '',
-                        );
-                        getSnackBar.show();
+                            Get.toNamed(Routes.OTP,
+                                arguments: {"email": email});
+                            // getSnackBar.show();
+                          } else {
+                            GetSnackBar getSnackBar = GetSnackBar(
+                              title: 'Email không tồn tại',
+                              subTitle: '',
+                            );
+                            getSnackBar.show();
+                          }
+                        });
                       }
-                    });
-                  },
-                )
-              ],
-            )));
+                    },
+                  )
+                ],
+              ))),
+    );
   }
 }
